cf4047ebd442f7414def91575680cc7817693618
Haziq Zikry <haziqzikry@gmail.com>
2023-06-04 10:42:17 +0800

    added navigation and side menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 
-// import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MyNavbarComponent } from './my-navbar/my-navbar.component';
+import { MySideMenuComponent } from './my-side-menu/my-side-menu.component';
+import { MyMainContentComponent } from './my-main-content/my-main-content.component';
+
+/**
+ * @title Basic buttons
+ */
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyNavbarComponent, MySideMenuComponent, MyMainContentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
